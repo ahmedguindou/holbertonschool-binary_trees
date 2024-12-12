171d96830a9c0a0055de0f701a18b07bb12177a3
ahmedguindou <guindouahmed@gmail.com>
2024-12-12 11:46:23 +0100

    binary_tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,5 +11,5 @@ if (tree == NULL)
 return (0);
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
-return (1+(left_height > right_height ? left_height : right_height));
+return (1 + (left_height > right_height ? left_height : right_height));
 }
